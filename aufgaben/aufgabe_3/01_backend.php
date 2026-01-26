4ec3399cd7a156cfe8eeb6b653639b832db31b8a
Anthony Bär <a.baer@baer-software.com>
2026-01-26 10:51:11 +0100

    Aufgabe 3 überarbeitet

--- aufgaben/aufgabe_3/01_backend.php
+++ aufgaben/aufgabe_3/01_backend.php
@@ -3,10 +3,7 @@
 // Ziel: POST-Parameter auslesen und sinnvoll ausgeben.
 
 // TODO:
-// 1) Lies $_POST['name'] und $_POST['alter'] aus.
-// 2) Wenn name oder alter fehlt: Gib eine Fehlermeldung aus.
-// 3) Wenn beide vorhanden sind: Begrüße die Person.
-//    Beispiel: "Hallo Lea, du bist 16 Jahre alt."
-
-// Optional:
-// - Prüfe mit is_numeric(), ob alter eine Zahl ist.
+// 1) Lies $_POST['benutzername'] und $_POST['passwort'] aus.
+// 2) Wenn ein Wert fehlt: Gib eine Fehlermeldung aus.
+// 3) Wenn beide vorhanden sind: Gebe eine Meldung mit dem Benutzername aus.
+//    Beispiel: "Hallo Benutzer! Du hast ein Passwort angegeben."
--- aufgaben/aufgabe_3/01_index.php
+++ aufgaben/aufgabe_3/01_index.php
@@ -1,12 +1,12 @@
 <?php
 // Aufgabe 3A: POST-Parameter (index.php)
-// Ziel: Ein Formular erstellen, das per POST an backend.php sendet.
+// Ziel: Ein Formular erstellen, das per POST an 01_backend.php sendet.
 
 // TODO:
-// 1) Erstelle ein HTML-Formular mit method="post" und action="backend.php".
+// 1) Erstelle ein HTML-Formular mit method="post" und action="01_backend.php".
 // 2) Felder:
-//    - name (Textfeld)
-//    - alter (Zahlenfeld)
+//    - benutzername (Textfeld)
+//    - Passwort (Passwort)
 
 // Hinweis:
 // - POST-Parameter sind nicht in der URL sichtbar.
